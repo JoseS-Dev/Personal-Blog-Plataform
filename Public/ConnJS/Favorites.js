16e2e25bb6520439449958e001b20f3b84c957aa
JoSantana0005 <joseasantana05@gmail.com>
2025-05-23 19:49:57 -0400

    map bro map bro jejeje

--- Public/ConnJS/Favorites.js
+++ Public/ConnJS/Favorites.js
@@ -1,5 +1,6 @@
 const FavoritesStorage = JSON.parse(localStorage.getItem('FavoritesBlogs')) || [];
 const FavoritesBlogs = document.getElementsByClassName('JS--Cards')[0];
+const DeleteBlog = document.getElementById('Delete--Notes');
 let ListBlog = [];
 let isDeleted = false;
 let selectedVisitedCard = null;
@@ -39,7 +40,7 @@ function getImageCategory(blog){
 function showFavoritesBlogs(){
     let favoritesContent = '';
     if(FavoritesStorage.length > 0){
-        ListBlog = FavoritesStorage.forEach((blog) => {
+        ListBlog = FavoritesStorage.map((blog) => {
             favoritesContent += `
                 <div class="JS--Card" id=${blog.idNotes}>
                     <div class="JS--ContentCardImage">
@@ -58,7 +59,7 @@ function showFavoritesBlogs(){
                 categoryBlog: blog.categoryBlog,
                 createdNotes: blog.createdNotes,
                 tagsBlog: blog.tagsBlog,
-                imageCategory: getImageCategory(blog)
+                ImageCategory: getImageCategory(blog)
             }
         })
         FavoritesBlogs.innerHTML = favoritesContent;
@@ -66,17 +67,43 @@ function showFavoritesBlogs(){
         favoritesContentCard.forEach((card) => {
             card.addEventListener('click', () => {
                 selectedVisitedCard = true;
+                const Index = card.getAttribute('id');
                 if(!isDeleted){
                     card.classList.toggle('JS--Card--IsVisited')
                     if(card.classList.contains('JS--Card--IsVisited')){
-                        const Index = card.getAttribute('id');
-                        window.location.href = `../Pages/BlogDetail.html?id=${Index}`;
-                        localStorage.setItem('BlogDetails', JSON.stringify(ListBlog.findIndex((blog) => blog.idNotes == Index)));
+                        console.log(Index);
+                        window.location.href = `../Pages/BlogDetails.html?id=${Index}`;
+                        localStorage.setItem('BlogDetails', JSON.stringify(ListBlog[ListBlog.findIndex((blog) => blog.idNotes == Index)]));
                         selectedVisitedCard = false;
+                    }
+                }
+                else{
+                    if(Index){
+                        card.classList.toggle('JS--Card--IsDeleted');
+                    }
+                    if(card.classList.contains('JS--Card--IsDeleted')){
+                        const blogIndex = FavoritesStorage.findIndex((blog) => blog.idNotes == Index);
+                        if(blogIndex !== -1){
+                            FavoritesStorage.splice(blogIndex, 1);
+                            alert('Blog is deleted from favorites');
+                            localStorage.setItem('FavoritesBlogs', JSON.stringify(FavoritesStorage));
+                            card.remove();
+                            isDeleted = false;
+                        }
+                        else{
+                            console.log('No se encontró el blog en favoritos');
+                        }
                     }
                 }
             })
         })
     }
 }
+
+// Function para eliminar los blogs de Favoritos
+DeleteBlog.addEventListener('click', () => {
+    isDeleted = true;
+    showFavoritesBlogs();
+})
 showFavoritesBlogs();
+console.log(ListBlog);
